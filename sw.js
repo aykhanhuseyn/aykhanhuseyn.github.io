d4f53123427d084170727a422c3bf80aac956d2f
Aykhan Huseyn <immense_ideas@icloud.com>
2019-10-31 13:37:45 +0400

    react added, dark mode switch for os dark mode preverence added, dark mode by cacke added

--- sw.js
+++ sw.js
@@ -1,29 +1,29 @@
-var cacheName = 'progressiveWebApp';
+var cacheName = 'AykhanHuseyn';
 var filesToCache = [
-    '/',
-    '/CurriculumVitae.html',
-    '/css/fonts.css',
-    '/css/cursors.css',
-    '/css/cVitae.css',
-    '/js/anime.min.js',
-    '/js/darkMode.js',
-    '/js/main.js'
+  '/',
+  '/CurriculumVitae.html',
+  '/css/fonts.css',
+  '/css/cursors.css',
+  '/css/cVitae.css',
+  '/js/anime.min.js',
+  '/js/darkMode.js',
+  '/js/main.js'
 ];
 
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
-    e.waitUntil(
-        caches.open(cacheName).then(function(cache) {
-            return cache.addAll(filesToCache);
-        })
-    );
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
 });
 
 /* Serve cached content when offline */
 self.addEventListener('fetch', function(e) {
-    e.respondWith(
-        caches.match(e.request).then(function(response) {
-            return response || fetch(e.request);
-        })
-    );
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
 });
